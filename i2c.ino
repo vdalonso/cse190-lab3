7561a750aca6e4f4bb3eafb2c555d15c972fdd4c
Victor Daniel Alonso <46736227+vdalonso@users.noreply.github.com>
2020-02-11 20:34:07 -0800

    Add files via upload

--- i2c.ino
+++ i2c.ino
@@ -17,13 +17,37 @@ void i2c_init(){
   /* ========== SERCOM INITIALIZATION =========== */
   SERCOM3->I2CM.CTRLA.reg |= SERCOM_I2CM_CTRLA_MODE_I2C_MASTER;
   SERCOM3->I2CM.BAUD.bit.BAUD = 59;
-
+  //enable the pins PA22 and PA23
   PORT->Group[0].PINCFG[22].bit.PMUXEN = 1;
   PORT->Group[0].PINCFG[23].bit.PMUXEN = 1;
 
   //select for (22, 23)/2 for function C
   PORT->Group[0].PMUX[22/2].reg = PORT_PMUX_PMUXE_C | PORT_PMUX_PMUXO_C ;
-
-  
   /* ============================================ */
   }
+  
+uint8_t i2c_transaction(uint8_t address, uint8_t dir, uint8_t* data, uint8_t len){
+   //load the address for the slave to ADDR with the direction bit at bit 0
+   SERCOM3->I2CM.ADDR.bit.ADDR = address << 1 | dir;
+   //dir == 1:  reading data
+   if(dir){
+     for(uint8_t i = 0; i < len ; i++){
+      while(!SERCOM3->I2CM.INTFLAG.bit.SB){}
+        //read data
+        data[i] = SERCOM3->I2CM.DATA.bit.DATA;
+        //write an ACK
+        SERCOM3->I2CM.STATUS.bit.RXNACK = 0;
+     }
+     //NOTE: on last byte, send NACK
+     SERCOM3->I2CM.STATUS.bit.RXNACK = 1;
+   }
+   //dir == 0: writing data
+   else{
+    for(uint8_t i = 1; i <= len ; i++){
+        while(!SERCOM3->I2CM.INTFLAG.bit.MB){}
+        //send data
+        SERCOM3->I2CM.DATA.bit.DATA = data[len-i];
+      }
+    }
+   
+}
